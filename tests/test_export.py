52b5d270bc8e7169db1e7d0c05519135cdf7aa4b
GitBib <bnff@redcollar.ru>
2021-10-18 21:41:29 +0300

    New tests added.

--- tests/test_export.py
+++ tests/test_export.py
@@ -3,9 +3,24 @@ from os.path import isfile
 
 
 def test_export(sub):
+    # Check output_dialogues
+    assert sub.export(output_dialogues=True)
+    assert not isfile("tests/sub.srt")
+
+    # Checking the default export
     sub.export()
     assert isfile("tests/sub.srt")
     os.remove("tests/sub.srt")
+
+    # Checking output_dir
     sub.export("tests/folder")
     assert isfile("tests/folder/sub.srt")
     os.remove("tests/folder/sub.srt")
+
+    # Checking compatibility
+    sub.export("tests/folder", "utf8", False)
+    assert isfile("tests/folder/sub.srt")
+    os.remove("tests/folder/sub.srt")
+
+    assert sub.export(None, "utf8", True)
+    assert not isfile("tests/sub.srt")
